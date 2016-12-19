f6e0db07f037b30e3cbdb6d346d0fffe1e0e2c70
ddoerffel <>
2016-12-19 11:09:06 +0000

    CR: T2016121190001552 (code merge of all packages - console script for stats created)

---
+++ Kernel/System/Console/Command/Admin/Stats/Import.pm
@@ -0,0 +1,58 @@
+# --
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::Console::Command::Admin::Stats::Import;
+
+use strict;
+use warnings;
+
+use base qw(Kernel::System::Console::BaseCommand);
+
+our @ObjectDependencies = (
+    'Kernel::System::Stats',
+);
+
+sub Configure {
+    my ( $Self, %Param ) = @_;
+
+    $Self->Description('Install Package Stats');
+    $Self->AddOption(
+        Name        => 'file-prefix',
+        Description => "Name of the file prefix which should be used.",
+        Required    => 1,
+        HasValue    => 1,
+        ValueRegex  => qr/(.*)/smx,
+    );
+
+    return;
+}
+
+sub PreRun {
+    my ( $Self, %Param ) = @_;
+
+    # check prefix
+    $Self->{FilePrefix} = $Self->GetOption('file-prefix');
+
+    return;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    $Self->Print("<yellow>Install stats with file prefix $Self->{FilePrefix}...</yellow>\n");
+
+    $Kernel::OM->Get('Kernel::System::Stats')->StatsInstall(
+        FilePrefix => $Self->{FilePrefix},
+        UserID     => 1,
+    );
+
+    $Self->Print("<green>Done.</green>\n");
+    return $Self->ExitCodeOk();
+}
+
+1;
