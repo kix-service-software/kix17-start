e99aa9ccba76f8f8b244683cd07d38dc4304afd3
ddoerffel <>
2016-12-19 13:03:48 +0000

    Bugfix: copyright added

--- Kernel/System/Console/Command/Admin/Stats/Import.pm
+++ Kernel/System/Console/Command/Admin/Stats/Import.pm
@@ -1,5 +1,9 @@
 # --
-# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# Copyright (C) 2006-2016 c.a.p.e. IT GmbH, http://www.cape-it.de
+#
+# written/edited by:
+# * Dorothea(dot)Doerffel(at)cape(dash)it(dot)de
+#
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
