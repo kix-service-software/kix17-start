5baa9470564238938d0d84a4a43856c2bfde176f
ddoerffel <>
2016-12-19 16:31:51 +0000

    CR: T2016121190001552 (code merge of all packages - adoptions from eXtended files added)

--- Kernel/System/ITSMConfigItem.pm
+++ Kernel/System/ITSMConfigItem.pm
@@ -394,13 +394,6 @@ add a new config item
 sub ConfigItemAdd {
     my ( $Self, %Param ) = @_;
 
-    # KIX4OTRS-capeIT
-    if ( $Self->SUPER::can('ConfigItemAdd') ) {
-        return $Self->SUPER::ConfigItemAdd(%Param);
-    }
-
-    # EO KIX4OTRS-capeIT
-
     # check needed stuff
     for my $Argument (qw(ClassID UserID)) {
         if ( !$Param{$Argument} ) {
@@ -429,6 +422,31 @@ sub ConfigItemAdd {
         );
         return;
     }
+
+    # KIX4OTRS-capeIT
+    # trigger ConfigItemCreate
+    my $Result = $Self->PreEventHandler(
+        Event => 'ConfigItemCreate',
+        Data  => {
+            ClassID => $Param{ClassID},
+            UserID  => $Param{UserID},
+            Version => $Param{Version} || '',
+        },
+        UserID => $Param{UserID},
+    );
+    if ( ( ref($Result) eq 'HASH' ) && ( $Result->{Error} ) ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'notice',
+            Message  => "Pre-ConfigItemAdd refused CI creation.",
+        );
+        return $Result;
+    }
+    elsif ( ref($Result) eq 'HASH' ) {
+        for my $ResultKey ( keys %{$Result} ) {
+            $Param{$ResultKey} = $Result->{$ResultKey};
+        }
+    }
+    # EO KIX4OTRS-capeIT
 
     # create config item number
     if ( $Param{Number} ) {
@@ -506,13 +524,6 @@ delete an existing config item
 sub ConfigItemDelete {
     my ( $Self, %Param ) = @_;
 
-    # KIX4OTRS-capeIT
-    if ( $Self->SUPER::can('ConfigItemDelete') ) {
-        return $Self->SUPER::ConfigItemDelete(%Param);
-    }
-
-    # EO KIX4OTRS-capeIT
-
     # check needed stuff
     for my $Argument (qw(ConfigItemID UserID)) {
         if ( !$Param{$Argument} ) {
@@ -528,6 +539,34 @@ sub ConfigItemDelete {
     my $ConfigItemData = $Self->ConfigItemGet(
         ConfigItemID => $Param{ConfigItemID},
     );
+
+    #---------------------------------------------------------------------------
+    # KIX4OTRS-capeIT
+    # trigger ConfigItemDelete
+    my $Result = $Self->PreEventHandler(
+        Event => 'ConfigItemDelete',
+        Data  => {
+            ConfigItemID => $Param{ConfigItemID},
+            Comment      => $Param{ConfigItemID},
+            UserID       => $Param{UserID},
+        },
+        UserID => $Param{UserID},
+    );
+    if ( ( ref($Result) eq 'HASH' ) && ( $Result->{Error} ) ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'notice',
+            Message  => "Pre-ConfigItemDelete refused CI deletion.",
+        );
+        return $Result;
+    }
+    elsif ( ref($Result) eq 'HASH' ) {
+        for my $ResultKey ( keys %{$Result} ) {
+            $Param{$ResultKey} = $Result->{$ResultKey};
+        }
+    }
+
+    # EO KIX4OTRS-capeIT
+    #---------------------------------------------------------------------------
 
     # delete all links to this config item first, before deleting the versions
     return if !$Kernel::OM->Get('Kernel::System::LinkObject')->LinkDeleteAll(
@@ -1831,6 +1870,542 @@ sub CurInciStateRecalc {
 
     return 1;
 }
+
+# KIX4OTRS-capeIT
+# Thefollowing methods are meant to ease the handling of the XML-LIKE data hash.
+# They do not replace any internal/original methods.
+
+=item GetAttributeValuesByKey()
+
+    Returns values first found, for a given attribute key.
+        _GetAttributeValuesByKey (
+            KeyName       => 'FQDN',
+            XMLData       => $XMLDataRef,
+            XMLDefinition => $XMLDefRef,
+        );
+=cut
+
+sub GetAttributeValuesByKey {
+    my ( $Self, %Param ) = @_;
+    my @RetArray = qw{};
+
+    # check required params...
+    if (
+        !$Param{KeyName}
+        ||
+        ( !$Param{XMLData} ) ||
+        ( !$Param{XMLDefinition} ) ||
+        ( ref $Param{XMLData} ne 'HASH' ) ||
+        ( ref $Param{XMLDefinition} ne 'ARRAY' )
+        )
+    {
+        return \@RetArray;
+    }
+
+    ITEM:
+    for my $Item ( @{ $Param{XMLDefinition} } ) {
+
+        COUNTER:
+        for my $Counter ( 1 .. $Item->{CountMax} ) {
+
+            # no content then stop loop...
+            last COUNTER if !defined $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{Content};
+
+            # check if we are looking for this key
+            if ( $Item->{Key} eq $Param{KeyName} ) {
+
+                # get the value...
+                my $Value = $Self->XMLValueLookup(
+                    Item  => $Item,
+                    Value => length( $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{Content} )
+                    ? $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{Content}
+                    : '',
+                );
+
+                if ($Value) {
+                    push( @RetArray, $Value );
+                }
+            }
+            next COUNTER if !$Item->{Sub};
+
+            #recurse if subsection available...
+            my $SubResult = $Self->GetAttributeValuesByKey(
+                KeyName       => $Param{KeyName},
+                XMLDefinition => $Item->{Sub},
+                XMLData       => $Param{XMLData}->{ $Item->{Key} }->[$Counter],
+            );
+
+            if ( ref($SubResult) eq 'ARRAY' ) {
+                for my $ArrayElem ( @{$SubResult} ) {
+                    push( @RetArray, $ArrayElem );
+                }
+            }
+        }
+    }
+
+    return \@RetArray;
+}
+
+=item GetAttributeContentsByKey()
+
+    Returns contents first found, for a given attribute key.
+        GetAttributeContentsByKey (
+            KeyName       => 'FQDN',
+            XMLData       => $XMLDataRef,
+            XMLDefinition => $XMLDefRef,
+        );
+=cut
+
+sub GetAttributeContentsByKey {
+    my ( $Self, %Param ) = @_;
+    my @RetArray = qw{};
+
+    # check required params...
+    if (
+        !$Param{KeyName}
+        ||
+        ( !$Param{XMLData} ) ||
+        ( !$Param{XMLDefinition} ) ||
+        ( ref $Param{XMLData} ne 'HASH' ) ||
+        ( ref $Param{XMLDefinition} ne 'ARRAY' )
+        )
+    {
+        return \@RetArray;
+    }
+
+    ITEM:
+    for my $Item ( @{ $Param{XMLDefinition} } ) {
+
+        COUNTER:
+        for my $Counter ( 1 .. $Item->{CountMax} ) {
+
+            # no content then stop loop...
+            last COUNTER if !defined $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{Content};
+
+            # get the value...
+            my $Content
+                = length( $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{Content} )
+                ? $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{Content}
+                : '';
+
+            if ( ( $Item->{Key} eq $Param{KeyName} ) && $Content ) {
+                push( @RetArray, $Content );
+            }
+
+            next COUNTER if !$Item->{Sub};
+
+            #recurse if subsection available...
+            my $SubResult = $Self->GetAttributeContentsByKey(
+                KeyName       => $Param{KeyName},
+                XMLDefinition => $Item->{Sub},
+                XMLData       => $Param{XMLData}->{ $Item->{Key} }->[$Counter],
+            );
+
+            if ( ref($SubResult) eq 'ARRAY' ) {
+                for my $ArrayElem ( @{$SubResult} ) {
+                    push( @RetArray, $ArrayElem );
+                }
+            }
+        }
+    }
+
+    return \@RetArray;
+}
+
+=item GetAttributeDataByType()
+
+    Returns a hashref with names and attribute values from the
+    XML-DataHash for a specified data type.
+
+    $ConfigItemObject->GetAttributeDataByType(
+        XMLData       => $XMLData,
+        XMLDefinition => $XMLDefinition,
+        AttributeType => $AttributeType,
+    );
+
+=cut
+
+sub GetAttributeDataByType {
+    my ( $Self, %Param ) = @_;
+
+    my @Keys = ();
+    my %Result;
+
+    #get all keys for specified input type...
+    @Keys = @{
+        $Self->GetKeyNamesByType(
+            XMLDefinition => $Param{XMLDefinition},
+            AttributeType => $Param{AttributeType}
+            )
+        };
+
+    if ( $Param{Content} ) {
+        for my $CurrKey (@Keys) {
+            my $CurrContent = $Self->GetAttributeContentsByKey(
+                KeyName       => $CurrKey,
+                XMLData       => $Param{XMLData},
+                XMLDefinition => $Param{XMLDefinition},
+            );
+            $Result{$CurrKey} = $CurrContent;
+        }
+    }
+    else {
+        for my $CurrKey (@Keys) {
+            my $CurrVal = $Self->GetAttributeValuesByKey(
+                KeyName       => $CurrKey,
+                XMLData       => $Param{XMLData},
+                XMLDefinition => $Param{XMLDefinition},
+            );
+            $Result{$CurrKey} = $CurrVal;
+        }
+    }
+
+    return \%Result;
+
+}
+
+=item GetKeyNamesByType()
+
+    Returns an array of keynames which are of a specified data type.
+
+    $ConfigItemObject->GetKeyNamesByType(
+        XMLDefinition => $XMLDefinition,
+        AttributeType => $AttributeType,
+    );
+
+=cut
+
+sub GetKeyNamesByType {
+    my ( $Self, %Param ) = @_;
+
+    my @Keys = ();
+    my %Result;
+
+    if ( defined( $Param{XMLDefinition} ) ) {
+
+        for my $AttrDef ( @{ $Param{XMLDefinition} } ) {
+            if ( $AttrDef->{Input}->{Type} eq $Param{AttributeType} ) {
+                push( @Keys, $AttrDef->{Key} )
+            }
+
+            next if !$AttrDef->{Sub};
+
+            my @SubResult = @{
+                $Self->GetKeyNamesByType(
+                    AttributeType => $Param{AttributeType},
+                    XMLDefinition => $AttrDef->{Sub},
+                    )
+                };
+
+            @Keys = ( @Keys, @SubResult );
+        }
+
+    }
+
+    return \@Keys;
+}
+
+=item _GetKeyNamesByType()
+
+    Sames as GetKeyNamesByType - returns an array of keynames which are of a
+    specified data type. => use GetKeyNamesByType instead !
+    !!! DEPRECATED - ONLY FOR COMPATIBILITY - WILL BE REMOVED !!!
+
+    $ConfigItemObject->_GetKeyNamesByType(
+        XMLDefinition => $XMLDefinition,
+        AttributeType => $AttributeType,
+    );
+
+=cut
+
+sub _GetKeyNamesByType {
+    my ( $Self, %Param ) = @_;
+
+    my @Keys = ();
+    my %Result;
+
+    if ( defined( $Param{XMLDefinition} ) ) {
+
+        for my $AttrDef ( @{ $Param{XMLDefinition} } ) {
+            if ( $AttrDef->{Input}->{Type} eq $Param{AttributeType} ) {
+                push( @Keys, $AttrDef->{Key} )
+            }
+
+            next if !$AttrDef->{Sub};
+
+            my @SubResult = @{
+                $Self->_GetKeyNamesByType(
+                    AttributeType => $Param{AttributeType},
+                    XMLDefinition => $AttrDef->{Sub},
+                    )
+                };
+
+            @Keys = ( @Keys, @SubResult );
+        }
+
+    }
+
+    return \@Keys;
+}
+
+=item GetAttributeDefByTagKey()
+
+Returns chosen CI attribute definition, for a given tag key.
+    my %AttrDef = $ConfigItemObject->GetAttributeDefByTagKey (
+        TagKey        => "[1]{'Version'}[1]{'Model'}[1]",
+        XMLData       => $XMLDataRef,
+        XMLDefinition => $XMLDefRef,
+    );
+
+returns
+
+    Input => {
+        Type
+        Class
+        Required
+        Translation
+        Size
+        MaxLength
+    }
+    Key
+    Name
+    Searchable
+    CountMin
+    CountMax
+    CountDefault
+
+=cut
+
+sub GetAttributeDefByTagKey {
+    my ( $Self, %Param ) = @_;
+
+    # check required params...
+    return
+        if (
+        !$Param{XMLData}       || ref( $Param{XMLData} )       ne 'HASH' ||
+        !$Param{XMLDefinition} || ref( $Param{XMLDefinition} ) ne 'ARRAY' ||
+        !$Param{TagKey}
+        );
+
+    ITEM:
+    for my $Item ( @{ $Param{XMLDefinition} } ) {
+        next ITEM if ( !$Param{XMLData}->{ $Item->{Key} } );
+
+        COUNTER:
+        for my $Counter ( 1 .. $Item->{CountMax} ) {
+            if ( $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{TagKey} eq $Param{TagKey} ) {
+                return %{$Item};
+            }
+
+            next COUNTER if !$Item->{Sub};
+
+            # recurse if subsection available...
+            my $SubResult = $Self->GetAttributeDefByTagKey(
+                TagKey        => $Param{TagKey},
+                XMLDefinition => $Item->{Sub},
+                XMLData       => $Param{XMLData}->{ $Item->{Key} }->[$Counter],
+            );
+
+            if ( $SubResult && ref($SubResult) eq 'HASH' ) {
+                return %{$SubResult};
+            }
+        }
+    }
+
+    return ();
+}
+
+=item VersionDataUpdate()
+
+    Returns a the current version data in the most current definition format
+    (usually this is done in the frontend). The version data might be structured
+    in a previous definition.
+
+    $NewVersionXMLData = $ConfigItemObject->VersionDataUpdate(
+        XMLDefinition => $NewDefinitionRef->{DefinitionRef},
+        XMLData       => $CurrentVersionRef->{XMLData}->[1]->{Version}->[1],
+    );
+
+=cut
+
+sub VersionDataUpdate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    return if !$Param{XMLData};
+    return if ref $Param{XMLData} ne 'HASH';
+    return if !$Param{XMLDefinition};
+    return if ref $Param{XMLDefinition} ne 'ARRAY';
+
+    my $FormData = {};
+
+    ITEM:
+    for my $Item ( @{ $Param{XMLDefinition} } ) {
+        my $CounterInsert = 1;
+
+        COUNTER:
+        for my $Counter ( 1 .. $Item->{CountMax} ) {
+
+            # create inputkey and addkey
+            my $InputKey = $Item->{Key} . '::' . $Counter;
+            if ( $Param{Prefix} ) {
+                $InputKey = $Param{Prefix} . '::' . $InputKey;
+            }
+
+            #get content...
+            my $Content = $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{Content} || '';
+            next COUNTER if !$Content;
+
+            # start recursion, if "Sub" was found
+            if ( $Item->{Sub} ) {
+                my $SubFormData = $Self->VersionDataUpdate(
+                    XMLDefinition => $Item->{Sub},
+                    XMLData       => $Param{XMLData}->{ $Item->{Key} }->[1],
+                    Prefix        => $InputKey,
+                );
+                $FormData->{ $Item->{Key} }->[$CounterInsert] = $SubFormData;
+            }
+
+            $FormData->{ $Item->{Key} }->[$CounterInsert]->{Content} = $Content;
+            $CounterInsert++;
+
+        }
+    }
+
+    return $FormData;
+}
+
+=item SetAttributeContentsByKey()
+
+    Sets the content of the specified keyname in the XML data hash.
+
+    $ConfigItemObject->SetAttributeContentsByKey(
+        KeyName       => 'Location',
+        NewContent    => $RetireCILocationID,
+        XMLData       => $NewVersionData,
+        XMLDefinition => $UsedVersion,
+    );
+
+=cut
+
+sub SetAttributeContentsByKey {
+    my ( $Self, %Param ) = @_;
+
+    # check required params...
+    if (
+        !$Param{KeyName}
+        ||
+        !length( $Param{NewContent} ) ||
+        ( !$Param{XMLData} ) ||
+        ( !$Param{XMLDefinition} ) ||
+        ( ref $Param{XMLData} ne 'HASH' ) ||
+        ( ref $Param{XMLDefinition} ne 'ARRAY' )
+        )
+    {
+        return 0;
+    }
+
+    ITEM:
+    for my $Item ( @{ $Param{XMLDefinition} } ) {
+
+        COUNTER:
+        for my $Counter ( 1 .. $Item->{CountMax} ) {
+
+            # get the value...
+            if ( $Item->{Key} eq $Param{KeyName} ) {
+                $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{Content} = $Param{NewContent};
+            }
+
+            # no content then stop loop...
+            last COUNTER if !defined $Param{XMLData}->{ $Item->{Key} }->[$Counter]->{Content};
+
+            next COUNTER if !$Item->{Sub};
+
+            #recurse if subsection available...
+            my $SubResult = $Self->SetAttributeContentsByKey(
+                KeyName       => $Param{KeyName},
+                NewContent    => $Param{NewContent},
+                XMLDefinition => $Item->{Sub},
+                XMLData       => $Param{XMLData}->{ $Item->{Key} }->[$Counter],
+            );
+
+        }
+    }
+
+    return 0;
+}
+
+=item VersionCount()
+
+    Returns the number of versions for a given config item.
+
+    $ConfigItemObject->VersionCount(
+        ConfigItemID => 123,
+    );
+
+=cut
+
+sub VersionCount {
+    my ( $Self, %Param ) = @_;
+    my $Result = 0;
+
+    return if ( !$Param{ConfigItemID} );
+
+    my $VersionList = $Self->VersionList(
+        ConfigItemID => $Param{ConfigItemID},
+    );
+    return if ( !$VersionList || ref($VersionList) ne 'ARRAY' );
+
+    $Result = ( scalar( @{$VersionList} ) || 0 );
+
+    return $Result;
+}
+
+=item CountLinkedObjects()
+
+Returns the number of objects linked with a given ticket.
+
+    my $Result = $ConfigItemObject->CountLinkedObjects(
+        ConfigItemID => 123,
+        UserID => 1
+    );
+
+=cut
+
+sub CountLinkedObjects {
+    my ( $Self, %Param ) = @_;
+    my $Result = 0;
+
+    return if ( !$Param{ConfigItemID} );
+
+    my $LinkObject = $Self->{LinkObject} || undef;
+
+    if ( !$LinkObject ) {
+        $LinkObject = $Kernel::OM->Get('Kernel::System::LinkObject');
+    }
+
+    return '' if !$LinkObject;
+
+    my %PossibleObjectsList = $LinkObject->PossibleObjectsList(
+        Object => 'ITSMConfigItem',
+        UserID => $Param{UserID} || 1,
+    );
+    for my $CurrObject ( keys(%PossibleObjectsList) ) {
+        my %LinkList = $LinkObject->LinkKeyList(
+            Object1 => 'ITSMConfigItem',
+            Key1    => $Param{ConfigItemID},
+            Object2 => $CurrObject,
+            State   => 'Valid',
+            UserID  => 1,
+        );
+
+        $Result = $Result + ( scalar( keys(%LinkList) ) || 0 );
+    }
+
+    return $Result;
+}
+
+# EO KIX4OTRS-capeIT
 
 =begin Internal:
 
