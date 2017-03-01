e578e77b51f587b9693878fc3143d07c113f871d
uboehm <>
2017-03-01 14:20:35 +0000

    Bugfix: T2016122190003148 (fix responsive layout)

--- CHANGES.md
+++ CHANGES.md
@@ -3,6 +3,7 @@
 * $Id$
 
 #17.0.0 (2017/xx/xx)
+ * (2017/03/01) - Bugfix: T2016122190003148 (fix responsive layout) (uboehm)
  * (2017/02/28) - Bugfix: T2017022790001227 (fixed vulnerability "possible cross-site scripting in parameter SelectedTab") (rbo)
  * (2017/02/28) - CR: T2016102690000501 (Updated BPMX files in KIX to the released version 5.0.3) (fjacquemin)
  * (2017/02/24) - CR: T2017022490001394 (better visualization of ticket locked message in case of RequiredLock) (rbo)
--- var/httpd/htdocs/js/Core.Agent.Responsive.js
+++ var/httpd/htdocs/js/Core.Agent.Responsive.js
@@ -52,26 +52,29 @@ Core.Agent.Responsive = (function (TargetNS) {
 
         // expand toolbar on click on the header
         $('#Header').off().on('click', function() {
+        	$('#ToolBar').toggleClass('toggle');
+            // KIXBase-capeIT
+            // deactivated with ticket 2016122190003148
+//               var TargetHeight = '70px',
+//                   TargetFade = 'out';
+   //
+//               if (parseInt($('#ToolBar').height(), 10) > 0) {
+//                   TargetHeight = '0px';
+//                   TargetFade = 'in';
+//               }
+//               $('#ToolBar').animate({
+//                   height: TargetHeight,
+//                   easing: 'easeOutQuart'
+//               }, 'fast');
 
-            var TargetHeight = '70px',
-                TargetFade = 'out';
-
-            if (parseInt($('#ToolBar').height(), 10) > 0) {
-                TargetHeight = '0px';
-                TargetFade = 'in';
-            }
-            $('#ToolBar').animate({
-                height: TargetHeight,
-                easing: 'easeOutQuart'
-            }, 'fast');
-
-            // fade sidebar handles in/out
-            if (TargetFade === 'in') {
-                $('.ResponsiveHandle').fadeIn();
-            }
-            else {
-                $('.ResponsiveHandle').fadeOut();
-            }
+               // fade sidebar handles in/out
+//               if (TargetFade === 'in') {
+//                   $('.ResponsiveHandle').fadeIn();
+//               }
+//               else {
+//                   $('.ResponsiveHandle').fadeOut();
+//               }
+            // EO KIXBase-capeIT
         });
 
         // wrap sidebar modules with an additional container
