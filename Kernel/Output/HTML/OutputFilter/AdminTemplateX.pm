b7fc881ce161a9b53f2c08ac9be61d9de91199fb
millinger <>
2017-03-16 13:19:21 +0000

    * (2017/03/16) - Bugfix: T2017031690001166 (moved output filter to correct folder) (millinger)

--- Kernel/Output/HTML/OutputFilter/AdminTemplateX.pm
+++ Kernel/Output/HTML/OutputFilter/AdminTemplateX.pm
@@ -6,7 +6,7 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::OutputFilterAdminTemplateX;
+package Kernel::Output::HTML::OutputFilter::AdminTemplateX;
 
 use strict;
 use warnings;
--- Kernel/Output/HTML/OutputFilter/AgentTemplateX.pm
+++ Kernel/Output/HTML/OutputFilter/AgentTemplateX.pm
@@ -6,7 +6,7 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::OutputFilterAgentTemplateX;
+package Kernel::Output::HTML::OutputFilter::AgentTemplateX;
 
 use strict;
 use warnings;
