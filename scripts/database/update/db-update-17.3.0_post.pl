edf7ed62796c93216f08acca29351217b465d58c
Rene Boehm <rene.boehm@cape-it.de>
2018-03-08 11:36:38 +0100

    Bugfix: T2018030690000568
    fixed problem with DB update from 17.2 to 17.3.0 when using MySQL or MariaDB

--- scripts/database/update/db-update-17.3.0_post.pl
+++ scripts/database/update/db-update-17.3.0_post.pl
@@ -37,29 +37,17 @@ exit 0;
 
 sub _MigrateDynamicFields {
 
-    # migrate object_id in dynamic_field_value - move values for type CustomerUser and CustomerCompany to new column object_id_text
-    my $List = $Kernel::OM->Get('Kernel::System::DynamicField')->DynamicFieldListGet(
-        Valid      => 0,
+    # get database object
+    my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
+
+    my $Success = $DBObject->Do(
+        SQL =>'UPDATE dynamic_field_value SET object_id = NULL WHERE object_id_text IS NOT NULL',
     );
-
-    if ($List) {
-        # get database object
-        my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
-
-        foreach my $DynamicField (@{$List}) {
-            next if ($DynamicField->{IdentifierDBAttribute} eq 'object_id');
-
-            my $Success = $DBObject->Do(
-                SQL =>'UPDATE dynamic_field_value SET object_id = NULL WHERE field_id = ?',
-                Bind => [ \$DynamicField->{ID} ]
-            );
-            if (!$Success) {
-                $Kernel::OM->Get('Kernel::System::Log')->Log(
-                    Priority => 'error',
-                    Message  => "Upable to erase object_id for DynamicField '$DynamicField->{Name}' ($DynamicField->{ID})!"
-                );
-            }
-        }
+    if (!$Success) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'error',
+            Message  => "Unable to erase object_id for DynamicFields with textual object_id!"
+        );
     }
 
     return 1;
