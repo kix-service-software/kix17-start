35df91e2a8f1e65cae83a5324bc0761cc1f8533d
Mario Illinger <mario.illinger@cape-it.de>
2019-05-03 13:16:53 +0200

    Bugfix: T2016081790001424
    prevent multiple executions of event handler modules

--- Kernel/Modules/CustomerTicketProcess.pm
+++ Kernel/Modules/CustomerTicketProcess.pm
@@ -13,6 +13,7 @@ package Kernel::Modules::CustomerTicketProcess;
 use strict;
 use warnings;
 
+use Kernel::System::VariableCheck qw(:all);
 use Kernel::Language qw(Translatable);
 
 our $ObjectManagerDisabled = 1;
