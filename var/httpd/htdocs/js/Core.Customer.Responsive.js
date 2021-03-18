1bb0ffd8e15c89dcaccc58f617f26fcc01e2c7f0
fjacquemin <fjacquemin@atlas>
2021-03-18 14:19:00 +0100

    Bugfix: KIX17-1771
    deactivated the switch function "ViewSwitchMode" in responsive mode

--- var/httpd/htdocs/js/Core.Customer.Responsive.js
+++ var/httpd/htdocs/js/Core.Customer.Responsive.js
@@ -54,14 +54,15 @@ Core.Customer.Responsive = (function (TargetNS) {
         });
 
         // Add switch for Desktopmode
-        if (!$('#ViewModeSwitch').length) {
+        // This function is deactivated with KIX17.15.
+        /*if (!$('#ViewModeSwitch').length) {
             $('#Footer').append('<div id="ViewModeSwitch"><a href="#">' + Core.Config.Get('ViewModeSwitchDesktop') + '</a></div>');
             $('#ViewModeSwitch a').on('click.Responsive', function() {
                 localStorage.setItem("DesktopMode", 1);
                 location.reload(true);
                 return false;
             });
-        }
+        }*/
 
         // wrap sidebar modules with an additional container
         $.each($('.SidebarColumn, #NavigationContainer'), function() {
