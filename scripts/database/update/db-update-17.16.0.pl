115673da9cdb14bd67e8881dad1a51446998e8b8
Frank Jacquemin <frank.jacquemin@cape-it.de>
2021-05-25 05:34:19 +0000

    CR: KIX17-2213
    updated jscolor library to 2.4.5

--- scripts/database/update/db-update-17.16.0.pl
+++ scripts/database/update/db-update-17.16.0.pl
@@ -45,6 +45,12 @@ sub _RemoveObsoleteDirectory {
 
     # prepare file list
     my @DirectoryList = (
+        'var/httpd/htdocs/js/thirdparty/canvg-1.4',
+        'var/httpd/htdocs/js/thirdparty/d3-3.5.6',
+        'var/httpd/htdocs/js/thirdparty/nvd3-1.7.1',
+        'var/httpd/htdocs/js/thirdparty/StringView-8',
+        'var/httpd/htdocs/skins/Agent/default/css/thirdparty/nvd3-1.7.1',
+
         'var/httpd/htdocs/js/thirdparty/jquery-2.1.4',
         'var/httpd/htdocs/js/thirdparty/jquery-migrate-1.2.1',
         'var/httpd/htdocs/js/thirdparty/jquery-tablesorter-2.0.5',
@@ -52,6 +58,10 @@ sub _RemoveObsoleteDirectory {
         'var/httpd/htdocs/js/thirdparty/jquery-validate-1.14.0',
         'var/httpd/htdocs/skins/Agent/default/css/thirdparty/ui-theme',
         'var/httpd/htdocs/skins/Customer/default/css/thirdparty/ui-theme',
+
+        'var/httpd/htdocs/js/thirdparty/jquery-jstree-3.1.1',
+        'var/httpd/htdocs/skins/Agent/default/css/thirdparty/jstree-theme',
+        'var/httpd/htdocs/skins/Customer/default/css/thirdparty/jstree-theme',
 
         'var/httpd/htdocs/js/thirdparty/jscolor_1.4.1',
 
