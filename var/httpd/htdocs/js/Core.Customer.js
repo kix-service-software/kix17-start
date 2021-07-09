49c635916c0277359735c4b976f1e361f076286a
Dorothea Doerffel <dorothea.doerffel@cape-it.de>
2021-07-09 10:09:06 +0000

    Bugfix: KIX17-2030
    translations added

--- var/httpd/htdocs/js/Core.Customer.js
+++ var/httpd/htdocs/js/Core.Customer.js
@@ -352,13 +352,32 @@ Core.Customer = (function (TargetNS) {
                 }
             });
             $.each($('.Field:has(*)'), function() {
+                var Empty = 1;
                 if (
-                    $(this).find(':visible').length
-                    || $(this).hasClass('Hidden')
+                    $(this).hasClass('Hidden')
+                    || $(this).hasClass('Options')
                 ) {
-                    return;
+                        return false;
                 }
-                $(this).addClass('FieldPlain');
+                $.each($(this).children(), function() {
+                    if (
+                        (
+                            $(this).is(':input')
+                            || $(this).hasClass('InputField_InputContainer')
+                            || $(this).find('#AttachmentUpload').length
+                        )
+                        && $(this).css('display') != 'none'
+                    ) {
+                        Empty = 0;
+                        return false;
+                    }
+                });
+                if ( Empty ) {
+                    $(this).addClass('FieldPlain');
+                }
+                else {
+                    $(this).removeClass('FieldPlain');
+                }
             });
         }
     };
