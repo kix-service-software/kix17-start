2cfd76b3890b73993e742482f743d6b32c914f5f
Mario Illinger <mario.illinger@cape-it.de>
2021-07-14 09:15:45 +0000

    Bugfix: KIX17-2387
    check connection to mail account before authentication

--- Kernel/System/MailAccount/IMAPTLS_OAuth2.pm
+++ Kernel/System/MailAccount/IMAPTLS_OAuth2.pm
@@ -1,18 +1,15 @@
 # --
 # Modified version of the work: Copyright (C) 2006-2021 c.a.p.e. IT GmbH, https://www.cape-it.de
 # based on the original work of:
-# Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2019–2021 Efflux GmbH, https://efflux.de/
 # Copyright (C) 2019-2021 Rother OSS GmbH, https://otobo.de/
 # --
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# This software comes with ABSOLUTELY NO WARRANTY. This program is
+# licensed under the AGPL-3.0 with code licensed under the GPL-3.0.
+# For details, see the enclosed files LICENSE (AGPL) and
+# LICENSE-GPL3 (GPL3) for license information. If you did not receive
+# this files, see https://www.gnu.org/licenses/agpl.txt (APGL) and
+# https://www.gnu.org/licenses/gpl-3.0.txt (GPL3).
 # --
 
 package Kernel::System::MailAccount::IMAPTLS_OAuth2;
@@ -45,6 +42,7 @@ sub Connect {
 
     my $Type = 'IMAP_OAuth2';
 
+### Code licensed under the GPL-3.0, Copyright (C) 2019-2021 Rother OSS GmbH, https://otobo.de/ ###
     # check needed stuff
     for (qw(OAuth2_ProfileID Login Password Host Timeout Debug)) {
         if ( !defined $Param{$_} ) {
@@ -55,10 +53,10 @@ sub Connect {
         }
     }
 
+    # get access token
     my $AccessToken = $Kernel::OM->Get('Kernel::System::OAuth2')->GetAccessToken(
         ProfileID => $Param{OAuth2_ProfileID}
     );
-
     if ( !$AccessToken ) {
         return (
             Successful => 0,
@@ -77,22 +75,50 @@ sub Connect {
         Ignoresizeerrors => 1,
     );
 
-    # Auth via SASL XOAUTH2.
+# KIX-capeIT, Copyright (C) 2006-2021 c.a.p.e. IT GmbH, https://www.cape-it.de
+    if ( !$IMAPObject ) {
+        return (
+            Successful => 0,
+            Message    => "$Type: Can't connect to $Param{Host}: $!!"
+        );
+    }
+# EO KIX-capeIT, Copyright (C) 2006-2021 c.a.p.e. IT GmbH, https://www.cape-it.de
+
+    # auth via SASL XOAUTH2
     my $SASLXOAUTH2 = encode_base64( 'user=' . $Param{Login} . "\x01auth=Bearer " . $AccessToken . "\x01\x01" );
     $IMAPObject->authenticate( 'XOAUTH2', sub { return $SASLXOAUTH2 } );
 
-    if ( !$IMAPObject || !$IMAPObject->IsAuthenticated() ) {
+    if ( !$IMAPObject->IsAuthenticated() ) {
         return (
             Successful => 0,
-            Message    => "$Type: Can't connect to $Param{Host}: $@\n"
+            Message    => "$Type: Auth for user $Param{Login}/$Param{Host} failed!"
         );
     }
 
     return (
         Successful => 1,
         IMAPObject => $IMAPObject,
+# KIX-capeIT, Copyright (C) 2006-2021 c.a.p.e. IT GmbH, https://www.cape-it.de
         Type       => $Type,
+# EO KIX-capeIT, Copyright (C) 2006-2021 c.a.p.e. IT GmbH, https://www.cape-it.de
     );
+### EO Code licensed under the GPL-3.0, Copyright (C) 2019-2021 Rother OSS GmbH, https://otobo.de/ ###
 }
 
 1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the KIX project
+(L<https://www.kixdesk.com/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. This program is
+licensed under the AGPL-3.0 with code licensed under the GPL-3.0.
+For details, see the enclosed files LICENSE (AGPL) and
+LICENSE-GPL3 (GPL3) for license information. If you did not receive
+this files, see <https://www.gnu.org/licenses/agpl.txt> (APGL) and
+<https://www.gnu.org/licenses/gpl-3.0.txt> (GPL3).
+
+=cut
