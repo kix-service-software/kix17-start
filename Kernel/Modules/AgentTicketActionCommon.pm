68467945c9381ca368ff4b6e1bba4faad8bdf299
Mario Bauer <mario.bauer@cape-it.de>
2022-06-22 14:16:26 +0200

    Bugfix: KIX17-3057
    handle Form-ACLs for article dynamic fields

--- Kernel/Modules/AgentTicketActionCommon.pm
+++ Kernel/Modules/AgentTicketActionCommon.pm
@@ -1461,8 +1461,6 @@ sub Run {
         DYNAMICFIELD:
         for my $DynamicFieldConfig ( @{ $Self->{DynamicField} } ) {
 
-            next DYNAMICFIELD if $DynamicFieldConfig->{ObjectType} ne 'Ticket';
-
             for my $DynamicFieldHTMLKey ( ('Label', 'Field') ) {
                 my $CurrentDynamicFieldHTML
                     = $DynamicFieldHTML{ $DynamicFieldConfig->{Name} }->{ $DynamicFieldHTMLKey };
